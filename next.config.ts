3258ae51a1a93c25180a1da9cac8c68ab75f68a6
bo_ <phanlakhon.ort+github@gmail.com>
2026-05-15 15:37:57 +0700

    fix: ui

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,13 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  ...(process.env.EXPORT === '1' && { output: 'export' }),
+  ...(process.env.EXPORT === '1' && { 
+    output: 'export',
+    basePath: '/ebooks/banpu-annual-report-2025',
+  }),
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withNextIntl(nextConfig);
